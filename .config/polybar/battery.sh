dad8f8b3655975d17a10e34a90dbf50199198158
Fabian Bläse <fabian@blaese.de>
2019-02-12 23:27:24 +0100

    Empty battery warning module

---
+++ .config/polybar/battery.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This function returns an awk script
+# Which prints the battery percentage
+# It's an ugly way to include a nicely indented awk script here
+get_awk_source() {
+cat <<EOF
+	BEGIN {
+		FS="=";
+	}
+\$1 ~ /ENERGY_FULL$/ {
+	f += \$2;
+}
+\$1 ~ /ENERGY_NOW\$/ {
+	n += \$2;
+}
+\$1 ~ /CHARGE_FULL$/ {
+	f += \$2;
+}
+\$1 ~ /CHARGE_NOW\$/ {
+	n += \$2;
+}
+END {
+	print int(100*n/f);
+}
+EOF
+}
+
+BATTERIES=/sys/class/power_supply/BAT*/uevent
+charge=$(cat $BATTERIES | awk -f <(get_awk_source))
+
+if [ $charge -lt 10 ]; then
+	exit 0
+else
+	exit 0
+fi
